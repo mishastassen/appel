fab99afc89afca4f772ac59f18ee615b55b85612
Jordi1080 <jordi.hesselz@hotmail.com>
2015-12-13 00:49:44 +0100

    Menu added (almost working)

---
+++ Appel/Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public void StartTheGame(){
+		Application.LoadLevel(1);
+		Debug.Log ("Started the game");
+	}
+
+	public void QuitTheGame(){
+		Application.Quit();
+		Debug.Log ("Quit the game");
+	}
+}
---
+++ Appel/Assets/Scripts/MenuStart.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuStart : MonoBehaviour {
+
+	// Use this for initialization
+	void StartThisGame () {
+		Application.LoadLevel("Level");
+	}
+
+}
