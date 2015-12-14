408c2fb80cfc115b5ea51c312b25b979f75adf11
pascal <pascalvdbosch@hotmail.com>
2015-12-14 02:55:14 +0100

    Help

--- Appel/Assets/Scripts/MenuController.cs
+++ Appel/Assets/Scripts/MenuController.cs
@@ -7,9 +7,15 @@ public class MenuController : MonoBehaviour {
 	public void StartTheGame(){
 		Application.LoadLevel(1);
 		Debug.Log ("Started the game");
-	}
+    }
 
-	public void QuitTheGame(){
+    public void StartHelp()
+    {
+        Application.LoadLevel(2);
+        Debug.Log("Helped the game");
+    }
+
+    public void QuitTheGame(){
 		Application.Quit();
 		Debug.Log ("Quit the game");
 	}
