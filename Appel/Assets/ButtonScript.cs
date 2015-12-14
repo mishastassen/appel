f30125670fe1d8d571b21b8c8f8e2ee9410718f3
pascal <pascalvdbosch@hotmail.com>
2015-12-14 03:21:14 +0100

    Coole naam

---
+++ Appel/Assets/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+   
+    public void GoBack()
+    {
+
+        Application.LoadLevel(0);
+        Debug.Log("Go back");
+    }
+}
